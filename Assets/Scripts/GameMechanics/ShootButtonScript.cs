879d2159f01dd24bb09584dee2badd0b066bea32
Leonhard Rist <leonhardrist@yahoo.de>
2019-07-14 22:15:15 +0200

    commit assets

--- Assets/Scripts/GameMechanics/ShootButtonScript.cs
+++ Assets/Scripts/GameMechanics/ShootButtonScript.cs
@@ -1,37 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
-public class Shoot : MonoBehaviour
+
+public class ShootButtonScript : MonoBehaviour
 {
     static Transform projectileSpawn;
     public GameObject spawnFromGameObject;
     public GameObject projectile;
     public float nextFire = 1.0f;
     public float currentTime = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         projectileSpawn = spawnFromGameObject.transform;
+        Debug.Log(projectileSpawn);
     }
 
     // Update is called once per frame
     void Update()
     {
-        shoot();
+        
     }
-
     public void shoot()
     {
         currentTime += Time.deltaTime;
-        bool button = CrossPlatformInputManager.GetButton("ShootButton");
-        Debug.Log(button);
-        if (button && currentTime > nextFire)
+
+        if (true)//(currentTime > nextFire)
         {
+            Debug.Log("Servus");
             nextFire += currentTime;
-            Instantiate(projectile, projectileSpawn);// Quaternion.FromToRotation(Vector3.right, transform.right));
+            Instantiate(projectile, projectileSpawn.position, Quaternion.FromToRotation(Vector3.right, transform.right));
             nextFire -= currentTime;
             currentTime = 0.0f;
         }
     }
+
 }
---
+++ Assets/Scripts/GameMechanics/rotateCrossHair.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class rotateCrossHair : MonoBehaviour
+{
+    public GameObject anchorObject;
+    static GameObject CrossHair;
+    static Vector3 anchor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CrossHair = GameObject.Find("CrossHair");
+        anchor = anchorObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float strength = CrossPlatformInputManager.GetAxis("Vertical");
+        Debug.Log(strength);
+
+        CrossHair.transform.RotateAround(anchorObject.transform.position, new Vector3(0, 0, 1), 1*strength);
+    }
+}
